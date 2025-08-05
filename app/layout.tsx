4167b0a3e50b5ca0a375977f07b9515baaacabfa
OM050498 <moyal232@gmail.com>
2025-08-05 14:16:37 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'אבי דהאן - הנדימן ואחזקה',
+  description: 'כל שירותי התיקונים/שיפוצים לבית שלך',
+  generator: 'OM1',
 }
 
 export default function RootLayout({
